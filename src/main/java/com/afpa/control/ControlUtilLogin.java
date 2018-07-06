e8ea4e5ddf96807ffed39c5efc3cc8da47ccf878
mavular <elkyach@yahoo.fr>
2018-07-06 11:42:47 +0200

    06/07/2018 V4

--- src/main/java/com/afpa/control/ControlUtilLogin.java
+++ src/main/java/com/afpa/control/ControlUtilLogin.java
@@ -14,24 +14,19 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.servlet.http.HttpServletRequest;
 import java.io.Serializable;
-import java.security.Principal;
 
 @Named
 @SessionScoped
-public class controlUtilLogin implements Serializable
+public class ControlUtilLogin implements Serializable
 {
-
     @Inject
     private AccessDb adb;
-
     private String messages;
     private String connectlabel;
     private Utilisateur u;
-
-    public controlUtilLogin()
+    public ControlUtilLogin()
     {
     }
-
     @PostConstruct
     public void init()
     {
@@ -39,36 +34,32 @@ public class controlUtilLogin implements Serializable
         connectlabel = "Se connecter";
         messages = "Non Connecté";
     }
-
-    public void ValidatePseudo(FacesContext context, UIComponent component,Object value) throws ValidatorException
+    public void ValidatePseudo(FacesContext context, UIComponent component, Object value) throws ValidatorException
     {
         if(adb.recherche(Utilisateur.class,value.toString())!=null)
         {
             throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_INFO,"Erreur","Le pseudo est déjà utilisé"));
         }
     }
-
-    public void logIn()
-    {
-        FacesContext context=FacesContext.getCurrentInstance(); //envoi de message
-        HttpServletRequest request=(HttpServletRequest) context.getExternalContext().getRequest(); //recuperation de la requete
-
-
-            if (u.getPseudo().equals(true) && u.getMotdepasse().equals(true)){
-            context.addMessage(null, new FacesMessage("Bienvenue " + u.getPseudo()));
-            u = adb.recherche(Utilisateur.class, u.getPseudo());
-
-            u.setMotdepasse(""); //le mot de passe est remis à blanc une fois la connection faite
-
-            messages="Connecté en tant que "+u.getPseudo(); //affiche le pseudo de l'utilisateur
-
-            connectlabel="Se déconnecter"; //change le libellé du bouton lorsque l'utilisateur est connecter
-        }
-
-            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN,"Le mot de passe n'est pas correct", ""));
-
-    }
-
+//    public void logIn()
+//    {
+//        FacesContext context=FacesContext.getCurrentInstance(); //envoi de message
+//        HttpServletRequest request=(HttpServletRequest) context.getExternalContext().getRequest(); //recuperation de la requete
+//
+//        try
+//        {
+//            //request.login(u.getPseudo(),u.getMotdepasse());
+//            context.addMessage(null, new FacesMessage("Bienvenue " + u.getPseudo()));
+//            u = adb.recherche(Utilisateur.class, u.getPseudo());
+//            u.setMotdepasse("");
+//            messages="Connecté en tant que "+u.getPseudo();
+//            connectlabel="Se déconnecter";
+//        }
+//        catch (ServletException svlet)
+//        {
+//            context.addMessage(null, new FacesMessage("Le mot de passe n'est pas correct"));
+//        }
+//    }
     public void show()
     {
         FacesContext context = FacesContext.getCurrentInstance();
@@ -78,49 +69,50 @@ public class controlUtilLogin implements Serializable
             System.out.println("show");
             RequestContext.getCurrentInstance().execute("PF('dlg1').show()");
         }
-        else
-        {
-            System.out.println("Deconnexion");
-
-            request.getSession().invalidate();
-
-        }
+//        else
+//        {
+//            System.out.println("Deconnexion");
+//            try{
+//                request.logout();
+//            }
+//            catch (ServletException ex)
+//            {
+//                Logger.getLogger(ControlUtilLogin.class.getName()).log(Level.SEVERE, null, ex);
+//                context.addMessage(null, new FacesMessage("Erreur de Déconnexion"));
+//            }
+//            request.getSession().invalidate();
+//            try
+//            {
+//                context.getExternalContext().redirect("accueil.xhtml");
+//            }
+//            catch (IOException ex)
+//            {
+//                Logger.getLogger(ControlUtilLogin.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//        }
     }
-
     public AccessDb getAdb() {
         return adb;
     }
-
     public void setAdb(AccessDb adb) {
         this.adb = adb;
     }
-
     public Utilisateur getU() {
         return u;
     }
-
     public void setU(Utilisateur u) {
         this.u = u;
     }
-
     public String getMessages() {
         return messages;
     }
-
     public void setMessages(String messages) {
         this.messages = messages;
     }
-
     public String getConnectlabel() {
         return connectlabel;
     }
-
     public void setConnectlabel(String connectlabel) {
         this.connectlabel = connectlabel;
     }
-
-    public Principal getCurrentUtilisateur(){
-        return ((HttpServletRequest)FacesContext.getCurrentInstance().getExternalContext().getRequest()).getUserPrincipal();
-    }
 }
-
